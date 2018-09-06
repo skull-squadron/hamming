16b83346f022ed8fdfc17798a88c44da64b9010d
Barry Allard <barry.allard@gmail.com>
2018-09-05 22:41:04 -0700

    travis: metalinker refactoring
    Signed-off-by: Barry Allard <barry.allard@gmail.com>

--- popcount_slices_amd64.go
+++ popcount_slices_amd64.go
@@ -1,13 +1,12 @@
 //
-// hamming distance calculations in Go
+// Package hamming distance calculations in Go
 //
 // https://github.com/steakknife/hamming
 //
-// Copyright © 2014, 2015, 2016 Barry Allard
+// Copyright © 2014, 2015, 2016, 2018 Barry Allard
 //
 // MIT license
 //
-
 package hamming
 
 import (
@@ -15,10 +14,19 @@ import (
 	"unsafe"
 )
 
+// CountBitsInt8sPopCnt count 1's in x
 func CountBitsInt8sPopCnt(x []int8) (ret int)
+
+// CountBitsInt16sPopCnt count 1's in x
 func CountBitsInt16sPopCnt(x []int16) (ret int)
+
+// CountBitsInt32sPopCnt count 1's in x
 func CountBitsInt32sPopCnt(x []int32) (ret int)
+
+// CountBitsInt64sPopCnt count 1's in x
 func CountBitsInt64sPopCnt(x []int64) (ret int)
+
+// CountBitsIntsPopCnt count 1's in x
 func CountBitsIntsPopCnt(x []int) int {
 	if strconv.IntSize == 64 {
 		y := (*[]int64)(unsafe.Pointer(&x))
@@ -29,10 +37,20 @@ func CountBitsIntsPopCnt(x []int) int {
 	}
 	panic("strconv.IntSize must be 32 or 64 bits")
 }
+
+// CountBitsUint8sPopCnt count 1's in x
 func CountBitsUint8sPopCnt(x []uint8) (ret int)
+
+// CountBitsUint16sPopCnt count 1's in x
 func CountBitsUint16sPopCnt(x []uint16) (ret int)
+
+// CountBitsUint32sPopCnt count 1's in x
 func CountBitsUint32sPopCnt(x []uint32) (ret int)
+
+// CountBitsUint64sPopCnt count 1's in x
 func CountBitsUint64sPopCnt(x []uint64) (ret int)
+
+// CountBitsUintsPopCnt count 1's in x
 func CountBitsUintsPopCnt(x []uint) int {
 	if strconv.IntSize == 64 {
 		y := (*[]uint64)(unsafe.Pointer(&x))
@@ -43,6 +61,12 @@ func CountBitsUintsPopCnt(x []uint) int {
 	}
 	panic("strconv.IntSize must be 32 or 64 bits")
 }
+
+// CountBitsBytesPopCnt count 1's in x
 func CountBitsBytesPopCnt(x []byte) (ret int)
+
+// CountBitsRunesPopCnt count 1's in x
 func CountBitsRunesPopCnt(x []rune) (ret int)
+
+// CountBitsStringPopCnt count 1's in s
 func CountBitsStringPopCnt(s string) (ret int)
